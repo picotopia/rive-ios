babf22d5d8e1ed6558afd128e7a77fa1a08b52c7
Zach Gotsch <orbipedis@gmail.com>
2024-03-31 19:48:02 -0700

    update package

--- Package.swift
+++ Package.swift
@@ -2,17 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "RiveRuntime",
-    platforms: [.iOS("14.0"), .macOS("13.1")],
-    products: [
-        .library(
-            name: "RiveRuntime",
-            targets: ["RiveRuntime"])],
-    targets: [
-        .binaryTarget(
-            name: "RiveRuntime",
-            url: "https://github.com/rive-app/rive-ios/releases/download/5.10.0/RiveRuntime.xcframework.zip",
-            checksum: "3afb59a77378a7e51cdf82a98b926581a3a548900017dc520984452d7a03bf4a"
-        ),
-    ]
+  name: "RiveRuntime",
+  platforms: [.iOS("14.0"), .macOS("13.1")],
+  products: [
+    .library(
+      name: "RiveRuntime",
+      targets: ["RiveRuntime"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "RiveRuntime",
+      url:
+        "https://github.com/picotopia/rive-ios/releases/download/blurclip-0.0.1/RiveRuntime.xcframework.zip",
+      checksum: "a10d464a57f4982fbbc6c989ab35615e34c9566cac94bedb34120c9683b1d8ad"
+    )
+  ]
 )
